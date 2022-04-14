e253d97fa652ee01abb4aa3cee5c4430e0aaa208
Batsaikhan Anand <batsaikhananand@LEAP-03s-iMac.local>
2022-04-14 16:20:20 +0800

    background main

--- kop.js
+++ kop.js
@@ -6,6 +6,42 @@ Array.from(items).forEach(item => {
     })
 });
 
-function GrowBox() {
-    document.getElementById("first").style.width = 600;
+function growBox1() {
+    document.getElementById("first").style.width = 400 + 'px';
+    document.getElementById("first").style.height = 600 + 'px';
+}
+
+function noGrowBox1() {
+    document.getElementById("first").style.width = 300 + 'px';
+    document.getElementById("first").style.height = 500 + 'px';
+}
+
+function growBox2() {
+    document.getElementById("second").style.width = 400 + 'px';
+    document.getElementById("second").style.height = 600 + 'px';
+}
+
+function noGrowBox2() {
+    document.getElementById("second").style.width = 300 + 'px';
+    document.getElementById("second").style.height = 500 + 'px';
+}
+
+function growBox3() {
+    document.getElementById("third").style.width = 400 + 'px';
+    document.getElementById("third").style.height = 600 + 'px';
+}
+
+function noGrowBox3() {
+    document.getElementById("third").style.width = 300 + 'px';
+    document.getElementById("third").style.height = 500 + 'px';
+}
+
+function growBox4() {
+    document.getElementById("fourth").style.width = 400 + 'px';
+    document.getElementById("fourth").style.height = 600 + 'px';
+}
+
+function noGrowBox4() {
+    document.getElementById("fourth").style.width = 300 + 'px';
+    document.getElementById("fourth").style.height = 500 + 'px';
 }
