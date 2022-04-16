8cb3e85b1abc2d00e5439343d851fabc48218609
Batsaikhan Anand <batsaikhananand@LEAP-03s-iMac.local>
2022-04-16 15:22:13 +0800

    some change bugs

---
+++ text-main-page.js
@@ -0,0 +1,59 @@
+const items = document.getElementsByClassName('item');
+const topBar = document.getElementById('top-bar');
+
+topBar.addEventListener('animationstart', () => {
+    topBar.classList.remove('hidden');
+});
+
+Array.from(items).forEach(item => {
+    item.addEventListener('animationstart', () => {
+        item.classList.remove('hidden');
+    })
+});
+
+var cursor = document.querySelector('.cursor');
+var cursor2 = document.querySelector('.cursor2');
+document.addEventListener('mousemove', function(e) {
+    cursor.style.cssText = cursor2.style.cssText = 'left: ' + e.clientX + 'px; top: ' + e.clientY + 'px;';
+})
+
+
+function growBox1() {
+    document.getElementById("first").style.width = 280 + 'px';
+    document.getElementById("first").style.height = 430 + 'px';
+}
+
+function noGrowBox1() {
+    document.getElementById("first").style.width = 250 + 'px';
+    document.getElementById("first").style.height = 400 + 'px';
+}
+
+function growBox2() {
+    document.getElementById("second").style.width = 280 + 'px';
+    document.getElementById("second").style.height = 430 + 'px';
+}
+
+function noGrowBox2() {
+    document.getElementById("second").style.width = 250 + 'px';
+    document.getElementById("second").style.height = 400 + 'px';
+}
+
+function growBox3() {
+    document.getElementById("third").style.width = 280 + 'px';
+    document.getElementById("third").style.height = 430 + 'px';
+}
+
+function noGrowBox3() {
+    document.getElementById("third").style.width = 250 + 'px';
+    document.getElementById("third").style.height = 400 + 'px';
+}
+
+function growBox4() {
+    document.getElementById("fourth").style.width = 280 + 'px';
+    document.getElementById("fourth").style.height = 430 + 'px';
+}
+
+function noGrowBox4() {
+    document.getElementById("fourth").style.width = 250 + 'px';
+    document.getElementById("fourth").style.height = 400 + 'px';
+}
