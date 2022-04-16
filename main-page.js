48a3c09b325e7a02f75813981c917e2e656f98ca
Batsaikhan Anand <batsaikhananand@LEAP-03s-iMac.local>
2022-04-16 16:50:30 +0800

    main page

--- main-page.js
+++ main-page.js
@@ -1,8 +1,13 @@
 const items = document.getElementsByClassName('item');
 const topBar = document.getElementById('top-bar');
+const botBar = document.getElementById('bottom-bar');
 
 topBar.addEventListener('animationstart', () => {
     topBar.classList.remove('hidden');
+});
+
+botBar.addEventListener('animationstart', () => {
+    botBar.classList.remove('hidden');
 });
 
 Array.from(items).forEach(item => {
@@ -13,10 +18,31 @@ Array.from(items).forEach(item => {
 
 var cursor = document.querySelector('.cursor');
 var cursor2 = document.querySelector('.cursor2');
-document.addEventListener('mousemove', function(e){
+document.addEventListener('mousemove', function(e) {
     cursor.style.cssText = cursor2.style.cssText = 'left: ' + e.clientX + 'px; top: ' + e.clientY + 'px;';
 })
 
+function changeColorDark() {
+    document.body.style.backgroundImage = "url('watercolor1.jpg')";
+    document.getElementById('light-mode').style.display = 'none';
+    document.getElementById('dark-mode').style.display = 'block';
+    const kolor = document.getElementsByClassName('kolor');
+    Array.from(kolor).forEach(item => {
+        item.style.color = 'white';
+    });
+
+}
+
+function changeColorLight() {
+    document.body.style.backgroundImage = "url('watercolor.jpg')";
+    document.getElementById('dark-mode').style.display = 'none';
+    document.getElementById('light-mode').style.display = 'block';
+    const kolor = document.getElementsByClassName('kolor');
+    Array.from(kolor).forEach(item => {
+        item.style.color = 'black';
+    });
+
+}
 
 function growBox1() {
     document.getElementById("first").style.width = 280 + 'px';
@@ -57,5 +83,3 @@ function noGrowBox4() {
     document.getElementById("fourth").style.width = 250 + 'px';
     document.getElementById("fourth").style.height = 400 + 'px';
 }
-
-
