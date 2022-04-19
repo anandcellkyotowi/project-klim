32786050c77338c53baa6ffee7884f08a4adfbc1
Batsaikhan Anand <batsaikhananand@LEAP-03s-iMac.local>
2022-04-19 17:13:04 +0800

    main

--- main-page.js
+++ main-page.js
@@ -78,3 +78,21 @@ function noGrowBox4() {
     document.getElementById("fourth").style.width = 250 + 'px';
     document.getElementById("fourth").style.height = 400 + 'px';
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
