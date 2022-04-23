14fa5567e1c784f00d23944982247a67c2681ce3
Uilsee <Munkh-Uils_@LEAP-22s-iMac.local>
2022-04-23 16:45:23 +0800

    uilsee's bulb

--- duuregin yums'/script2.js
+++ duuregin yums'/script2.js
@@ -19,43 +19,59 @@ const block10 = document.querySelector(".block10");
 var LightOn = true;
 
 lightBulb.addEventListener("click", () => {
-    if (LightOn) {
-        backGround.style.backgroundColor = "rgb( 40, 40, 40)";
-        LightOn = false;
-        hidden1.style.right = "9%";
-        hidden2.style.left = "10%";
-        hidden3.style.left = "57%";
-        hidden4.style.top = "110vh";
-        box2.style.top = "75vh";
-        lightBulb.src = "lightoff.png";
-        block1.style.height = "320px";
-        block2.style.height = "285px";
-        block3.style.height = "260px";
-        block4.style.height = "235px";
-        block5.style.height = "210px";
-        block6.style.height = "185px";
-        block7.style.height = "160px";
-        block8.style.height = "135px";
-        block9.style.height = "110px";
-        block10.style.height = "85px";
-    } else {
-        backGround.style.backgroundColor = "#3d463a";
-        LightOn = true;
-        hidden1.style.right = "-50%";
-        hidden2.style.left = "-50%";
+  if (LightOn) {
+    backGround.style.backgroundColor = "rgb( 40, 40, 40)";
+    LightOn = false;
+    hidden1.style.right = "9%";
+    hidden2.style.left = "10%";
+    hidden3.style.left = "57%";
+    hidden4.style.top = "110vh";
+    box2.style.top = "75vh";
+    lightBulb.src = "lightoff.png";
+    block1.style.height = "320px";
+    block2.style.height = "285px";
+    block3.style.height = "260px";
+    block4.style.height = "235px";
+    block5.style.height = "210px";
+    block6.style.height = "185px";
+    block7.style.height = "160px";
+    block8.style.height = "135px";
+    block9.style.height = "110px";
+    block10.style.height = "85px";
+    function myFunction(x) {
+      if (x.matches) {
+        box2.style.top = "110vh";
+        box2.style.width = "80%";
+        hidden4.style.width = "80%";
+        hidden4.style.top = "140vh";
+        hidden3.style.width = "80%";
+        hidden3.style.top = "210vh";
         hidden3.style.left = "10%";
-        hidden4.style.top = "75vh";
-        box2.style.top = "46vh";
-        lightBulb.src = "lighton.png";
-        block1.style.height = "0px";
-        block2.style.height = "0px";
-        block3.style.height = "0px";
-        block4.style.height = "0px";
-        block5.style.height = "0px";
-        block6.style.height = "0px";
-        block7.style.height = "0px";
-        block8.style.height = "0px";
-        block9.style.height = "0px";
-        block10.style.height = "0px";
+      } else {
+      }
     }
+
+    var x = window.matchMedia("(max-width: 1300px)");
+    myFunction(x);
+    x.addListener(myFunction);
+  } else {
+    backGround.style.backgroundColor = "#3d463a";
+    LightOn = true;
+    hidden1.style.right = "-50%";
+    hidden2.style.left = "-50%";
+    hidden3.style.left = "10%";
+    hidden4.style.top = "75vh";
+    box2.style.top = "46vh";
+    lightBulb.src = "lighton.png";
+    block1.style.height = "0px";
+    block2.style.height = "0px";
+    block3.style.height = "0px";
+    block4.style.height = "0px";
+    block5.style.height = "0px";
+    block6.style.height = "0px";
+    block7.style.height = "0px";
+    block8.style.height = "0px";
+    block9.style.height = "0px";
+    block10.style.height = "0px";
+  }
 });
