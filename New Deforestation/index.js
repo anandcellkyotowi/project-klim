073c8621e42cd19f9499787d9128fac4101ff8ed
duuree17 <duuren0315@gmail.com>
2022-04-25 00:03:08 +0800

    Duurenbayr Update 4,24

---
+++ New Deforestation/index.js
@@ -0,0 +1,16 @@
+const img1 = document.getElementById('img1')
+const img2 = document.getElementById('img2')
+
+
+function aiaiaiaiai() {
+    requestAnimationFrame(aiaiaiaiai)
+    if (innerWidth <= 670) {
+        img1.src = 'll.jpeg';
+        img2.src = 'forest1.png';
+    } else {
+        img1.src = 'pp.jpeg'
+        img2.src = 'gg.jpeg'
+    }
+}
+
+aiaiaiaiai()
