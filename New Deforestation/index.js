9beae715eb274716d012eb6bc9585f29c40291c3
duurenbayr <duurnbayr@niggers.local>
2022-04-26 17:12:57 +0800

    jotarosudo git add .

--- New Deforestation/index.js
+++ New Deforestation/index.js
@@ -1,16 +1,21 @@
-const img1 = document.getElementById('img1')
-const img2 = document.getElementById('img2')
-
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
 
 function aiaiaiaiai() {
-    requestAnimationFrame(aiaiaiaiai)
-    if (innerWidth <= 670) {
-        img1.src = 'll.jpeg';
-        img2.src = 'forest1.png';
-    } else {
-        img1.src = 'pp.jpeg'
-        img2.src = 'gg.jpeg'
-    }
+  requestAnimationFrame(aiaiaiaiai);
+  if (innerWidth <= 670) {
+    img1.src = "ll.jpeg";
+    img2.src = "forest1.png";
+  } else {
+    img1.src = "pp.jpeg";
+    img2.src = "gg.jpeg";
+  }
 }
-
-aiaiaiaiai()
+function backFunc() {
+  var my_url = "../main-page2.html";
+  setTimeout(() => {
+    window.location.replace(my_url);
+    console.log("yonfoo");
+  }, 0);
+}
+aiaiaiaiai();
