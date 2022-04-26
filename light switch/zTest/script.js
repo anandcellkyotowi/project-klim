be38e0a764cf9180ff476b6229fd4e93237f7e50
Batsaikhan Anand <batsaikhananand@LEAP-03s-iMac.local>
2022-04-26 17:05:02 +0800

    l

--- light switch/zTest/script.js
+++ light switch/zTest/script.js
@@ -35,3 +35,8 @@ window.addEventListener("scroll", reveal);
 
 
 reveal();
+var cursor = document.querySelector('.cursor');
+var cursor2 = document.querySelector('.cursor2');
+document.addEventListener('mousemove', function(e) {
+    cursor.style.cssText = cursor2.style.cssText = 'left: ' + e.clientX + 'px; top: ' + e.clientY + 'px;';
+})
