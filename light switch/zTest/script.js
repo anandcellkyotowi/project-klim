5d863767d097a4931b62ebece89ee5309476280b
Uilsee <Munkh-Uils_@LEAP-22s-iMac.local>
2022-05-03 18:59:09 +0800

    air pollution zasalt

--- light switch/zTest/script.js
+++ light switch/zTest/script.js
@@ -1,11 +1,14 @@
 const lightBulb = document.querySelector('.axe')
 const backGround = document.querySelector('.container')
 const shit = document.querySelector('.infoish')
+const touch = document.querySelector('.touch')
 var LightOn = true
 
 lightBulb.addEventListener("click", () => {
     if (LightOn) {
         backGround.style.backgroundColor = "rgb( 40, 40, 40)";
+        shit.style.display = "block"
+        touch.style.display = "none"
         shit.style.marginLeft = '0'
         LightOn = false;
         lightBulb.src = "lightoff.png";
@@ -13,7 +16,8 @@ lightBulb.addEventListener("click", () => {
     } else {
         backGround.style.backgroundColor = "#3D463A";
         LightOn = true;
-        shit.style.marginLeft = '100%'
+        shit.style.display = "none"
+        touch.style.display = "block"
         lightBulb.src = "lighton.png";
     }
 });
