0fa530d69455e1a7e4b1d0d199ec391a232ff088
Batsaikhan Anand <batsaikhananand@LEAP-03s-iMac.local>
2022-05-03 17:45:02 +0800

    main-page2

--- main-page2.js
+++ main-page2.js
@@ -1,3 +1,15 @@
+const items = document.getElementsByClassName('item');
+const topBar = document.getElementById('top-bar');
+topBar.addEventListener('animationstart', () => {
+    topBar.classList.remove('hidden');
+});
+
+Array.from(items).forEach(item => {
+    item.addEventListener('animationstart', () => {
+        item.classList.remove('hidden');
+    })
+});
+
 var cursor = document.querySelector('.cursor');
 var cursor2 = document.querySelector('.cursor2');
 document.addEventListener('mousemove', function(e) {
@@ -44,21 +56,3 @@ function noGrowBox4() {
     document.getElementById("fourth").style.width = 300 + 'px';
     document.getElementById("fourth").style.height = 300 + 'px';
 }
-
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-window.addEventListener("scroll", reveal);
-
-
-reveal();
