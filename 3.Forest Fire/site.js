18a83fc46291326b9ee121d97091572f0b5a8de9
M.Munkh-Orgil <Monkhoo17@Nest-100s-iMac.local>
2022-05-12 17:14:56 +0800

    firesmoke

--- 3.Forest Fire/site.js
+++ 3.Forest Fire/site.js
@@ -8,10 +8,18 @@ document.addEventListener("mousemove", function (e) {
 var ciggar = document.getElementById("ciggar");
 var button = document.getElementById("button");
 var text = document.getElementById("info");
+var backG = document.getElementById("backG");
+var smoke = document.getElementById("smoke");
 
 function myFunction() {
-  ciggar.style.width = 150 + "px";
+  // ciggar.style.width = 150 + "px";
+  // ciggar.style.backgroundColor = "rgb(92, 92, 92)";
+  text.innerHTML =
+    "Уур амьсгалын өөрчлөлт, ойн доройтол, хуваагдал нь дэлхий даяар түймэрт өртөмтгий нөхцөл байдлыг бий болгоод байна. Илүү халуун, хуурай нөхцөлд хүнээс эсвэл аянга цахилгаанаар гал авалцдаг гал нь илүү том талбайд, илүү халуун температурт шатах магадлал өндөр байдаг.";
   button.style.opacity = 0;
   text.style.opacity = 1;
-  ciggar.style.backgroundColor = "rgb(92, 92, 92)";
+  ciggar.classList.remove("burn");
+  backG.classList.remove("pageani");
+  backG.style.backgroundColor = "rgb(255, 124, 48)";
+  smoke.style.opacity = 0;
 }
