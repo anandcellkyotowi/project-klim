ed72d8474d25ffad82a147d8462e82b95d7130a7
Batsaikhan Anand <batsaikhananand@LEAP-03s-iMac.local>
2022-05-19 16:17:10 +0800

    quiz

--- public/Quiz/quiz.js
+++ public/Quiz/quiz.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
     measurementId: "G-0W77FD2HC8"
 };
 let questions = []
+let cnt = 0;
 initializeApp(firebaseConfig)
 const db = getFirestore()
 const colRef = collection(db, 'questions')
@@ -76,16 +77,21 @@ function resetState() {
 }
 
 function selectAnswer(e) {
+    console.log(e);
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
     setStatusClass(document.body, correct)
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
+            // } else if (selectedButton = ) {
+
     } else {
         questionContainerElement.style.display = 'none';
+        console.log(cnt);
         resultElement.innerHTML = 'Манай асуулт хариултын тоглоомыг тоглосонд танд баярлалаа!'
         startButton.innerText = 'Ахин тоглох'
         startButton.classList.remove('hide')
@@ -106,81 +112,3 @@ function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
-// const questions = [{
-//         question: 'Эдгээрийн аль нь дэлхий цаг уурын өөрчлөлтөөс гарж буй үр дагавар вэ?',
-//         answers: [
-//             { text: 'Мөсөн уулнууд хайлж далай өмнөхөөсөө илүү хүчиллэг болж байна ', correct: false },
-//             { text: 'Гадаргуйн дулаан жил тутам дээд рекорд тогтоож байна', correct: false },
-//             { text: 'Олон төрлийн амьтад устах ( Ган гачиг, далайн шуурга г.м)', correct: false },
-//             { text: 'Бүгд зөв', correct: true }
-//         ]
-//     },
-//     {
-//         question: 'Эдгээр улсуудын аль нь хамгын их нүүрстөрөгчийн давхар исэл гаргадаг вэ?',
-//         answers: [
-//             { text: 'АНУ', correct: false },
-//             { text: 'Их Британи', correct: false },
-//             { text: 'ОХУ', correct: false },
-//             { text: 'БНХАУ', correct: true }
-//         ]
-//     },
-//     {
-//         question: 'Доорхи он жилүүдээс алинд нь халууны дээд рекорд тогтоогдсон бэ?',
-//         answers: [
-//             { text: '2016', correct: false },
-//             { text: '2020', correct: false },
-//             { text: 'Дээрхи Бүгд', correct: true },
-//             { text: 'Аль нь ч биш', correct: false }
-//         ]
-//     },
-//     {
-//         question: 'Эдгээрээс аль нь дэлхийн дулаарлын гол шалтгаан нь вэ?',
-//         answers: [
-//             { text: 'Задарч буй ургамалууд', correct: false },
-//             { text: 'Шатаж буй тос , нүүрс , бензин', correct: true },
-//             { text: 'Байгаль өөрөө дулаарч байгаа', correct: false },
-//             { text: 'Түймрийн улмаас үүссэн бохирдол', correct: false }
-//         ]
-//     },
-//     {
-//         question: 'Сүүлийн 100 жилд эдгээр газруудын аль нь хамгийн их дулаарсан бэ?',
-//         answers: [
-//             { text: 'Марракеч, Морокко', correct: false },
-//             { text: 'Басра, Ирак', correct: false },
-//             { text: 'Ванкоуер, Канад', correct: false },
-//             { text: 'Свалбард, Норвеги', correct: true }
-//         ]
-//     },
-//     {
-//         question: 'Сүүлийн үед цаг агаарын өөрчлөлтийн нөлөө нь аль бүсүүдэд илүү ажиглагдсан бэ?',
-//         answers: [
-//             { text: 'Хойд өргөрөг ', correct: true },
-//             { text: 'Өмнөд өргөрөг', correct: false },
-//             { text: 'Халуун Орны бүс нутаг ', correct: false },
-//             { text: 'Бүх газруудад ижил тархсан', correct: false }
-//         ]
-//     },
-//     {
-//         question: 'Дэлхийн дулаарал дэлхийн бүх хэсгүүдэд ижил нөлөөлдөг',
-//         answers: [
-//             { text: 'Үнэн', correct: false },
-//             { text: 'Худал', correct: true },
-//         ]
-//     },
-//     {
-//         question: 'Цаг агаарын өөрчлөлт болон эрс тэс уур амьсгал холбоотой',
-//         answers: [
-//             { text: 'Үнэн', correct: true },
-//             { text: 'Худал', correct: false },
-//         ]
-//     },
-//     {
-//         question: 'Сүүлчийн 100 жилд далайн түвшин хэдэн СМ өссөн бэ?',
-//         answers: [
-//             { text: '5', correct: false },
-//             { text: '17.7', correct: true },
-//             { text: '40.6', correct: false },
-//             { text: '12.7', correct: false }
-//         ]
-//     },
-// ]
